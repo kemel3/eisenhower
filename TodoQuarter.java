8ca57ea5475b76117aed023f9998d32e94f5947e
KaristonVeluraz <kg97@interia.pl>
2019-10-18 22:32:13 +0200

    add item sort sort sort

--- TodoQuarter.java
+++ TodoQuarter.java
@@ -13,8 +13,18 @@ public class TodoQuarter {
     public void addItem(String title, LocalDate deadline) {
         TodoItem td = new TodoItem(title, deadline);
         todoItems.add(td);
-        Comparator <TodoItem> c = Collections.reverseOrder();
-        todoItems.sort(c);
+        //Comparator <TodoItem> c = Collections.reverseOrder();
+        // todoItems.sort(c);
+        int arrLen = todoItems.size();
+        LocalDate localDate = LocalDate.now();
+        //LocalDate pom = todoItems.get(1).deadline;
+        ArrayList<TodoItem> pom = new ArrayList<TodoItem>();
+        LocalDate neareDate = todoItems.get(0).deadline;
+        for (int i = 0; i < arrLen; i++){
+            
+            
+
+        }
     }
 
     public void removeItem(int index) {
