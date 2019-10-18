3dbe31b72c54d31da45b14242c4dc80113be1209
Kemel3 <kemelZX@gmail.com>
2019-10-18 13:53:42 +0200

    Add TodoMatrix to TodoMatrix

--- TodoMatrix.java
+++ TodoMatrix.java
@@ -1,38 +1,36 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.nio.Buffer;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.io.FileReader;
 
 public class TodoMatrix {
     
+    Map <String, TodoQuarter> todoQuarters = new HashMap <String, TodoQuarter>();
+    
+    public TodoMatrix(){
+        todoQuarters.put("IU", new TodoQuarter());
+        todoQuarters.put("IN", new TodoQuarter());
+        todoQuarters.put("NU", new TodoQuarter());
+        todoQuarters.put("NN", new TodoQuarter());
+    }
 
-    Map <String, TodoQuarter> todoQuarters = new Map <String,TodoQuarter>();
-
-    //todoQuarters.put();
-
-
-
-    private String getQuarters() {
+    public Map <String, TodoQuarter> getQuarters() {
         return todoQuarters;
     }
 
-    public String getQuarter(String status) {
-
-
+    public TodoQuarter getQuarter(String status) {
         return todoQuarters.get(status);
     }
 
-    public String addItem(String title, LocalDate deadline, boolean isImportant) {
+    public void addItem(String title, LocalDate deadline, boolean isImportant) {
         
        // todoQuarters.addItem()
 
     }
 
-    public void addItemsFromFile(String fileName){
+    public void addItemsFromFile(String fileName) throws Exception{
 
         File file = new File(fileName);
         BufferedReader br = new BufferedReader(new FileReader(file));
