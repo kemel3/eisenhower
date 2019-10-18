2fbb5817b5495a6111665032b17346683f888a48
KaristonVeluraz <kg97@interia.pl>
2019-10-18 11:17:35 +0200

    Add metods to matrix file addItemsFromFile

--- TodoMatrix.java
+++ TodoMatrix.java
@@ -1,17 +1,60 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.nio.Buffer;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.io.FileReader;
 
 public class TodoMatrix {
-    Map todoQuarters; 
+    
+
+    Map <String, TodoQuarter> todoQuarters = new Map<String,TodoQuarter>();
+
+    //todoQuarters.put();
+
+
 
     private String getQuarters() {
         return todoQuarters;
     }
 
     public String getQuarter(String status) {
-        return get(todoQuarters);
+
+
+        return todoQuarters.get(status);
     }
 
-    public Strng addItem(String title, LocalDate deadline, boolean isImportant) {
+    public String addItem(String title, LocalDate deadline, boolean isImportant) {
         
+       // todoQuarters.addItem()
+
     }
+
+    public void addItemsFromFile(String fileName){
+
+        File file = new File(fileName);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+        while((line = br.readLine()) != null){
+            String[] variables = line.split("|");
+            String title = variables[0];
+            LocalDate deadline = LocalDate.parse(variables[1]);
+            boolean isImportant = variables[2];
+
+            addItem(title, deadline, isImportant);
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+
 }
